7a8e5f6fba0d4af0fab198723b8eca9baa80fe6c
Complexlity <nwalozielijah@gmail.com>
2024-07-01 11:34:10 +0100

    feat: update contract

--- nftDetails/nft.sol
+++ nftDetails/nft.sol
@@ -9,10 +9,10 @@ contract Burnpfs is  ERC721URIStorage, Ownable {
     uint256 public totalSupply;
     uint256 public maxSupply;
     bool public isMintEnabled;
-    string private _baseContractURI = "https://peach-worldwide-trout-968.mypinata.cloud/ipfs/QmPAVixDZMWHEoWweiPQQciUvKogvbwSafNF77XgFzPjmE";
-    string private _baseTokenURI = "https://example.com/";
+    string private _baseContractURI = "https://peach-worldwide-trout-968.mypinata.cloud/ipfs/QmbYFe2NRcNwRaMskXPgTevSP2GnUGw7YUk13PHP2ZRdSq";
+    string private _baseTokenURI = "https://farcasterxarbitrum.vercel.app/api/xmetadata/";
     mapping (string => uint256) private fidToTokenId;
-        
+
     constructor(address initialOwner) payable ERC721('farcasterXarbitrum', 'FXA') Ownable(initialOwner){
         maxSupply = 2**256 - 1;
     }
@@ -31,10 +31,13 @@ contract Burnpfs is  ERC721URIStorage, Ownable {
     }
 
 
-     function setbaseURI(string memory _newBaseURI) external  onlyOwner returns (string memory) {
-        return _baseContractURI = _newBaseURI;
+     function setbaseContractURI(string memory _newBaseContractURI) external  onlyOwner returns (string memory) {
+        return _baseContractURI = _newBaseContractURI;
     }
-    
+     function setbaseURI(string memory _newBaseTokenURI) external  onlyOwner returns (string memory) {
+        return _baseTokenURI = _newBaseTokenURI;
+    }
+
      function contractURI() external view returns (string memory) {
         return _baseContractURI;
     }
@@ -53,14 +56,14 @@ contract Burnpfs is  ERC721URIStorage, Ownable {
     }
 
     function mint(string memory fid) external payable {
-        require(msg.value == mintPrice, "Wrong value");   
-        internalMint(msg.sender, fid);
+        require(msg.value == mintPrice, "Wrong value");
+        _internalMint(msg.sender, fid);
     }
     function mintFor(address user, string memory fid) external onlyOwner {
-        internalMint(user, fid);
+        _internalMint(user, fid);
     }
 
-    function internalMint(address _addr, string memory fid) internal {
+    function _internalMint(address _addr, string memory fid) internal {
         require(isMintEnabled, "Mint must be enabled");
         require(balanceOf(_addr) < 1, "Cannot own more than one NFT");
         totalSupply++;
