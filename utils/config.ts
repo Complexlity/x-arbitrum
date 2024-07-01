e4008bac3c9d7b4cfbb239ade75b8b2a2f76eb8d
Complexlity <nwalozielijah@gmail.com>
2024-07-01 17:50:13 +0100

    feat: swap zod for arktype

--- utils/config.ts
+++ utils/config.ts
@@ -1,28 +1,40 @@
 
-// import {  ArkErrors, type } from 'arktype'
+import {  ArkErrors, type } from 'arktype'
 import z from 'zod'
 import { config as dotEnvConfig } from 'dotenv'
+import { isAddress } from 'viem'
 
 dotEnvConfig({ path: "./.env" })
 
+const startsWithOx = type("string").narrow((address, ctx) => {
+  if (isAddress(address)) {
+    return true;
+  }
+  // add a customizable error and return false
+  return ctx.mustBe("A valid EVM address");
+});
 
-// const parseConfig = type({
-//     REDIS_URL: "url>0",
-//     REDIS_TOKEN: "string>0",
-//     AIRSTACK_API_KEY: "string>0"
+
+const configParser = type({
+    REQUIRED_NFT_ADDRESS: startsWithOx,
+    HOST: "url",
+  })
+  // AIRSTACK_API_KEY: "string>0"
+// const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+
+const config = configParser(process.env)
+
+// if()
+// const configParser = z.object({
+//   REQUIRED_NFT_ADDRESS: z.string().startsWith("0x"),
+//   HOST: z.string().url()
+
 // })
-const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
 
-
-const configParser = z.object({
-  REQUIRED_NFT_ADDRESS: z.string().startsWith("0x"),
-  HOST: z.string().url()
-
-})
-
-//Make process.env types match as needed
-// type EnvSchemaType = typeof parseConfig.infer
-type EnvSchemaType = z.infer<typeof configParser>
+// Make process.env types match as needed
+type EnvSchemaType = typeof configParser.infer
+// type EnvSchemaType = z.infer<typeof configParser>
 
 declare global {
   namespace NodeJS {
@@ -30,11 +42,14 @@ declare global {
   }
 }
 
-const config = configParser.parse(process.env)
+// const config = configParser.parse(process.env)
+if (config instanceof type.errors) {
+  throw new Error(config.summary)
+}
 
-export {config}
+// export {config}
 
-//     type T = Exclude<typeof config, ArkErrors>
-// const configg = config as T
-// export {configg as config}
+  type T = Exclude<typeof config, ArkErrors>
+  const configg = config as T
+export { configg as config }
 
